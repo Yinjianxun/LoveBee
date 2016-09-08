f005246ac10eef86487540bdda3fa0e819ffbec6
yinjianxun <ss_768@163.com>
2016-09-08 10:43:40 +0800

    添加工程

---
+++ FreshDi/FreshDi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreshDi
+//
+//  Created by Yin jianxun on 16/9/8.
+//  Copyright © 2016年 YinJianxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreshDi/FreshDi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FreshDi
+//
+//  Created by Yin jianxun on 16/9/8.
+//  Copyright © 2016年 YinJianxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
